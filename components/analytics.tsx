3f47c2d080f9eea3a4750d214c212406f55cb864
GenTrackSA <ruan.jacobs91@gmail.com>
2025-11-12 11:25:07 +0200

    Update analytics.tsx

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -4,13 +4,9 @@ import Script from "next/script";
 export default function Analytics() {
   const id = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   if (!id) return null;
-
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
-        strategy="afterInteractive"
-      />
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${id}`} strategy="afterInteractive" />
       <Script id="ga-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
